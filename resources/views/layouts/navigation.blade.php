80ba12e9873e73a84bcb21db8f3e2e29880f03c4
rayys0703 <rayys0703@gmail.com>
2024-10-31 16:24:00 +0700

    Memperbaiki Navigation Blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Net2mart</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/logo_ptjaringsolusi.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
     <!-- jQuery -->
