850695eb6a8d37434090a8db7ababed6250ac861
rayys0703 <rayys0703@gmail.com>
2024-11-05 14:35:59 +0700

    Dashboard - Memperbaiki bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1266,8 +1266,8 @@
                     <div class="d-flex align-items-center justify-content-center round-48 rounded text-bg-warning flex-shrink-0 mb-3 mx-auto">
                       <iconify-icon icon="solar:inbox-in-linear" class="fs-7 text-white"></iconify-icon>
                     </div>
-                    <h6 class="fw-normal fs-3 mb-1">Stok Barang</h6>
-                    <h4 id="stok_barang" class="mb-3 d-flex align-items-center justify-content-center gap-1">{{ $total_barang_masuk }}</h4>
+                    <h6 class="fw-normal fs-3 mb-1">Barang Masuk</h6>
+                    <h4 id="stok_barangs" class="mb-3 d-flex align-items-center justify-content-center gap-1">{{ $total_barang_masuk }}</h4>
                     <a href="{{ route('laporan.stok') }}" class="btn btn-white fs-2 fw-semibold text-nowrap">View
                       Details</a>
                   </div>
@@ -2340,7 +2340,7 @@
                     document.querySelector('#stok_seluruh').textContent = stokKeseluruhan.toLocaleString();
 
                     // Update stok barang di HTML Stok Barang
-                    document.querySelector('#stok_barang').textContent = stokKeseluruhan.toLocaleString();
+                    // document.querySelector('#stok_barang').textContent = stokKeseluruhan.toLocaleString();
 
                     // Hitung surplus/defisit dari stok kemarin
                     const stokYesterday = stokPerhari[stokPerhari.length - 2]?.jumlah || 0;
