ea13f81392dbc9cb060ea00451a2f098133a75d5
rayys0703 <rayys0703@gmail.com>
2024-12-04 12:34:59 +0700

    Memperbarui Nama Bulan pada BAST

--- app/Http/Controllers/BASTController.php
+++ app/Http/Controllers/BASTController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PhpOffice\PhpWord\TemplateProcessor;
 use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
 
 class BASTController extends Controller
 {
     public function index($id)
     {
-        // Mendapatkan data permintaan barang keluar
         if (!app('permission')->can('item request.viewAll')) {
             $response = Http::withToken(session('jwt_token'))->get(config('app.api_url') . "/permintaanbarangkeluar/onlyfor");
         } else {
@@ -21,13 +21,9 @@ class BASTController extends Controller
         $allData = $permintaan_barang_keluar['data'];
         $filteredData = collect($allData)->firstWhere('id', $id);
         
-        // Mendapatkan detail barang dari permintaan barang keluar
         $barang_details = Http::withToken(session('jwt_token'))->get(config('app.api_url') . "/permintaanbarangkeluar/{$id}")->json();
-
-        // Mendapatkan serial number detail dari permintaan barang keluar
         $serial_numbers = Http::withToken(session('jwt_token'))->get(config('app.api_url') . "/permintaanbarangkeluar/show-detail-sn/{$id}")->json();
 
-        // Membuat TemplateProcessor untuk file DOCX
         $templateProcessor = new TemplateProcessor(public_path('templates/bast.docx'));
 
         // Mengisi data umum ke template
@@ -46,7 +42,7 @@ class BASTController extends Controller
         $templateProcessor->setValue('${tahun}', $tahun);
 
         // $tanggal_keluar = date('d F Y', strtotime($filteredData['updated_at']));
-        $tanggal_keluar = date('d F Y');
+        $tanggal_keluar = Carbon::now()->translatedFormat('d F Y');
         $templateProcessor->setValue('${tanggal_keluar}', $tanggal_keluar);
 
         $project = $filteredData['ba_project'] ?? null;
@@ -55,15 +51,12 @@ class BASTController extends Controller
         $no_po = $filteredData['ba_no_po'] ?? null;
         $templateProcessor->setValue('${no_po}', $no_po);
 
-        // Membuat data untuk tabel yang akan di-clone
         $tableRows = [];
         $no = 1;
         foreach ($barang_details as $barang) {
-            // Mengambil serial number untuk barang tertentu
             $serialNums = collect($serial_numbers)->where('barang_id', $barang['barang_id'])->pluck('serial_number')->all();
             $serialNumbersString = implode(', ', $serialNums);
 
-            // Mengisi data untuk satu baris
             $tableRows[] = [
                 'no' => $no,
                 'nama_barang' => $barang['nama_barang'],
@@ -73,15 +66,12 @@ class BASTController extends Controller
             $no++;
         }
 
-        // Menggandakan baris dalam tabel di Word sesuai dengan jumlah data
         $templateProcessor->cloneRowAndSetValues('no', $tableRows);
 
-        // Menyimpan dokumen Word yang dihasilkan
         $fileName = "BAST - {$filteredData['nama_customer']} - {$tanggal_keluar}.docx";
         $tempFilePath = storage_path("app/public/{$fileName}");
         $templateProcessor->saveAs($tempFilePath);
 
-        // Mengembalikan response untuk mendownload file
         return response()->download($tempFilePath)->deleteFileAfterSend(true);
     }
 }
